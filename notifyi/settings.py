cebeb3b8149e6a698f35f805cc149d25d902ee17
Suraj Goel <goel.suraj5@gmail.com>
2021-07-08 02:17:21 +0100

    stuff

--- notifyi/settings.py
+++ notifyi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1pb%zd+1&2nw5+1a)m$*22tefex*s-)tz5s89qn41b&c_qhdzh'
-
+try:
+    SECRET_KEY=DJANGO_SECRET_KEY
+except:
+     SECRET_KEY=os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
