cd155b7216ebb99a6e454ce0716e7bb7618bb428
Suraj Goel <goel.suraj5@gmail.com>
2021-07-17 23:24:34 +0100

    favicon

--- accounts/views.py
+++ accounts/views.py
@@ -13,10 +13,10 @@ def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user=form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=raw_password)
+            #user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('user_home')
     else:
