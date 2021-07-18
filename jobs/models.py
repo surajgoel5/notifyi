d0d3ab3868c85ba2d34bd7c96ca891e9525cb2ba
Suraj Goel <goel.suraj5@gmail.com>
2021-07-18 03:41:35 +0100

    added logo, AND HOME STARTED APP

--- jobs/models.py
+++ jobs/models.py
@@ -20,7 +20,7 @@ class Job(models.Model):
 
     def send_notif(self):
         icon = static('/favicon.png')
-        badge = 'https://notifyi.herokuapp.com/static/badge.png'#static('/badge.png')
+        badge = static('/badge.png')
        # print(badge)
         payload = {"head": self.title, "body": self.message,"icon":icon, "badge": badge}
         send_group_notification(group_name=self.account.user.username, payload=payload, ttl=1000)
